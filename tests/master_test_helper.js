31e5c92acf89367d063fbffd8cdff9fb5088544c
Waseem Akram <waseem@wazemtech.dev>
2026-05-23 17:06:05 +0500

    🧹 Clean up repository (remove vsix and temporary debug files)

--- tests/master_test_helper.js
+++
@@ -1,3 +0,0 @@
-const { spawnSync } = require('child_process');
-const res = spawnSync('node', ['wate.js', '--watch', 'tests/tmp_cache.wate'], { timeout: 1000 });
-console.log(res.stdout ? res.stdout.toString() : '');
